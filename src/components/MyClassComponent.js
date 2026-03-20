ff19103821093b23dc01fe260c9f82a4c4bbdac5
SambitAutomation <77014127+SambitAutomation@users.noreply.github.com>
2026-03-20 17:04:11 +0530

    class component added

---
+++ src/components/MyClassComponent.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class MyClassComponent extends Component{
+
+    render(){
+        return(
+            <h1> My New Class Component</h1>
+        )
+    }
+}
+
+export default MyClassComponent;
--- src/components/MyComponent2.js
+++ src/components/MyComponent2.js
@@ -1,10 +1,11 @@
 import React from "react"
+import MyClassComponent from "./MyClassComponent"
 
 export const MyComponent2 = () => {
 
     return(
         <React.Fragment>
-            <span>My new Fragment 2</span>
+            <span>My new Fragment 2 Holding <MyClassComponent /> </span>
         </React.Fragment>
     )
 }
